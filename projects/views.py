56b52cb1478f268677f3d098768c02823991a083
sy106 <yuanshen106@qq.com>
2019-09-26 14:36:20 +0800

    初始化项目

--- projects/views.py
+++ projects/views.py
@@ -24,26 +24,30 @@ class IndexView(View):
     """
     def get(self,request):
         #get 请求
-        # return HttpResponse("<h1>GET请求:Hello,python!</h1>")
+        #1\使用request.GET来获取查询字符串参数
+        #2、request.GET返回的是一个 字典类的对象，支持字典中的所有操作
+        #3、查询字符串参数中，如果有使用多个相同的key,使用request.GET获取的是最后一个值
+        #4、使用request.GET.getlist('name')可以获取多个相同key值的参数
+        return HttpResponse("<h1>GET请求:Hello,python!</h1>")
         #从数据库中读取数据
-        datas =[
-            {
-                'project_name':'前程贷项目',
-                'leader':'可优',
-                'app_name':'P2P平台应用'
-            },
-            {
-                'project_name': '探索火星项目',
-                'leader': '优优',
-                'app_name': '吊炸天项目'
-            },
-            {
-                'project_name': '务必牛逼的项目',
-                'leader': '可可',
-                'app_name': '神秘应用'
-            },
-        ]
-        return  render(request,'index.html',locals())
+        # datas =[
+        #     {
+        #         'project_name':'前程贷项目',
+        #         'leader':'可优',
+        #         'app_name':'P2P平台应用'
+        #     },
+        #     {
+        #         'project_name': '探索火星项目',
+        #         'leader': '优优',
+        #         'app_name': '吊炸天项目'
+        #     },
+        #     {
+        #         'project_name': '务必牛逼的项目',
+        #         'leader': '可可',
+        #         'app_name': '神秘应用'
+        #     },
+        # ]
+        # return  render(request,'index.html',locals())
 
     def post(self, request):
         # get 请求
